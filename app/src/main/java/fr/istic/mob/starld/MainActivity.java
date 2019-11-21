0e83773b93c634ca90d7635657c25f31e16ffc35
Yaelle <yaelle.dubois04@orange.fr>
2019-11-21 16:21:20 +0100

    Do the begin of IHM

--- app/src/main/java/fr/istic/mob/starld/MainActivity.java
+++ app/src/main/java/fr/istic/mob/starld/MainActivity.java
@@ -2,20 +2,73 @@ package fr.istic.mob.starld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     Intent intent;
+    TextView chooseHour;
+    TextView chooseDate;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        intent = new Intent(getApplicationContext(), ServiceStar.class);
+        chooseHour = findViewById(R.id.textViewHour);
+        chooseDate = findViewById(R.id.textViewDate);
+        calendar = GregorianCalendar.getInstance();
+        this.initializeTextView();
 
-        startService(intent);
+        //intent = new Intent(getApplicationContext(), ServiceStar.class);
+
+        //startService(intent);
     }
+
+    private void initializeTextView () {
+        chooseHour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minutes) {
+                        chooseHour.setText(hour + " : "+minutes);
+                    }
+                }, calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE), true);
+                timePickerDialog.show();
+            }
+        });
+
+        chooseDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int annee, int mois, int jour) {
+                        chooseDate.setText(jour + " " + mois+ " "+annee);
+                    }
+                },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.show();
+            }
+        });
+    }
+
+
 }
