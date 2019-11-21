58bba6a58bec5566187db6ffefb8a63baf14435b
Bolbi <bolbi.ket@gmail.com>
2019-11-21 16:41:31 +0100

    WIP

--- app/src/main/java/fr/istic/mob/starld/ServiceStar.java
+++ app/src/main/java/fr/istic/mob/starld/ServiceStar.java
@@ -8,19 +8,25 @@ import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
+import android.widget.Toast;
 
 
 public class ServiceStar extends Service {
+    String url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-busmetro-horaires-gtfs-versions-td";
+    Looper serviceLooper;
+    ServiceStarHandler serviceStarHandler;
 
     @Override
     public void onCreate(){
         HandlerThread threadServiceStar = new HandlerThread("ThreadStarService", Process.THREAD_PRIORITY_BACKGROUND);
         threadServiceStar.start();
+        serviceLooper = threadServiceStar.getLooper();
+        serviceStarHandler = new ServiceStarHandler(serviceLooper);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
-
+        Toast.makeText(this, "Service running", Toast.LENGTH_SHORT).show();
         return START_STICKY;
     }
 
